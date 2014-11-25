7316592d2beb3c58fdbb0aeaff52da973f863d08
Fernando <Fernando@Fernando>
2014-11-24 22:01:32 -0600

    primera

--- proyectFinal/src/proyectfinal/ProyectFinal.java
+++ proyectFinal/src/proyectfinal/ProyectFinal.java
@@ -15,8 +15,15 @@ public class ProyectFinal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
         
+        //ya pude desde netbeans
+       
+        inventario inventario = new inventario ();
+        
+        inventario.incluirProducto(1234, "Arroz", 5, 2000, 10);
+        inventario.incluirProducto("12/9/15", 15, 12345, "Leche", 100, 500, 2);
+        
+        System.out.println(inventario.listaProductos());
     }
     
 }
